96025bbec613a3127d2e54d7b5be6136ec4ec2ef
joshy <joshy@localhost>
2012-02-28 21:09:22 -0800

    merge

--- tools/joshdoc.js
+++ tools/joshdoc.js
@@ -26,6 +26,10 @@ function escape(str) {
     return str;
 }
 
+function w(s) {
+    console.log(s);
+}
+
 function parseData2(data) {
     var docs = {classes:[]};
     data = data.toString();
@@ -36,63 +40,49 @@ function parseData2(data) {
     while(true) {
         var res = regex.exec(data);
         if(res == null) break;
-        //console.log(res[1]);
+        
         if(res[1] == "overview") {
             var end = regex.exec(data);
             docs.overview = escape(data.substring(res.index,end.index));
         }
+        
         if(res[1] == "class") {
             var end = regex.exec(data);
             var cls = data.substring(res.index,end.index);
             
             var r = /@class\s*(\w+)\s*(.*)/m;
             var r2 = r.exec(cls);
-            //console.log("name = " + r2[1]);
-            
-            currentClass = {name:r2[1],description:cls, functions:[]};
+            currentClass = {
+                name:r2[1],
+                description:cls, 
+                functions:[], 
+                properties:[],
+            };
             docs.classes.push(currentClass);
         }
+        
         if(res[1] == "function") {
-            var r = /@function\s*(\w+)\s*(.*)/gm;
+            //var r = /@function\s*(\w+)\s*(.*)/gm;
+            var r = /@function\s*(\w+)\s*(\(\S*\))*\s*(.*)/gm;
             r.lastIndex = res.index;
             var r2 = r.exec(data);
-            //console.log("r2 = " + res.index + " " + r2[1]);
-            var fun = { name: r2[1], description: r2[2]};
+            var fun = { name: r2[1], args:r2[2], description: r2[3]};
             currentClass.functions.push(fun);
         }
-    }
-    return docs;
-}
-
-
-function parseData(data) {
-    var docs = {classes:[]};
-    var regex = new RegExp("@overview([\\s\\S]*?)@end","m");
-    docs.overview = escape(regex.exec(data)[1]);
-    
-    
-    var class_regex = /@class\s*(\w+)([\s\S]*?)@end/mg;
-    while(true) {
-        var result = class_regex.exec(data);
-        if(result == null) break;
-        //console.log(result[2]);
         
-        var cls = {name:result[1], description:result[2]};
-        
-        var fun = /@function\s*(\w+)\s+(.*)/g;
-        while(true) {
-            var funresult = fun.exec(data);
-            if(funresult == null) break;
-            cls.functions = [];
-            cls.functions.push({name:funresult[1],description:funresult[2]});
+        if(res[1] == "property") {
+            var r = /@property\s*(\w+)\s*(.*)/gm;
+            r.lastIndex = res.index;
+            var r2 = r.exec(data);
+            var fun = { name: r2[1], description: r2[2]};
+            currentClass.properties.push(fun);
         }
         
-        docs.classes.push(cls);
-        
     }
-    
     return docs;
 }
+
+
 
 var docs = {
     overview: "nothing",
@@ -109,37 +99,61 @@ var docs = {
 }
 
 
-//generateHTML(docs);
 
-function w(s) {
-    console.log(s);
-}
 function generateHTML(docs) {
     w("<html>");
     w("<head><link rel='stylesheet' href='style.css'></link></head>");
     w("<body>");
-/*    
+   
+    w("<ul id='nav'>");
+    for(var n in docs.classes) {
+        var cls = docs.classes[n];
+        w("<li><a href='#"+cls.name+"'>"+cls.name+"</a></li>");
+    }
+    w("</ul>");
+    
     w("<pre id='overview'>");
     w(docs.overview);
     w("</pre>");
-*/    
+    
     
     for(var n in docs.classes) {
+        
         var cls = docs.classes[n];
-        w("<h1>"+cls.name+"</h1>");
+        w("<h2><a id='"+cls.name+"'>"+cls.name+"</a></h2>");
         w("<div class='description'>");
         w(cls.description);
         w("</div>");
         
-        w("<h3>functions</h3>");
-        w("<ul>");
-        for(var f in cls.functions) {
-            w("<li>");
-            w("<b>" + cls.functions[f].name +"</b>");
-            w(cls.functions[f].description);
-            w("</li>");
+        if(cls.functions.length > 0) {
+            w("<h3>functions</h3>");
+            w("<ul>");
+            for(var f in cls.functions) {
+                w("<li>");
+                w("<span>");
+                w("<b>" + cls.functions[f].name +"</b>");
+                if(cls.functions[f].args) {
+                    w("<i>" + cls.functions[f].args +"</i>"); 
+                }
+                w("</span>");
+                w(cls.functions[f].description);
+                w("</li>");
+            }
+            w("</ul>");
         }
-        w("</ul>");
+        
+        if(cls.properties.length > 0) {
+            w("<h3>properties</h3>");
+            w("<ul>");
+            for(var f in cls.properties) {
+                w("<li>");
+                w("<b>" + cls.properties[f].name +"</b>");
+                w(cls.properties[f].description);
+                w("</li>");
+            }
+            w("</ul>");
+        }
+        
     }
     
     w("</body></html>");
